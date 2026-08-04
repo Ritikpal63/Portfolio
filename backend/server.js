72b709bca0c58379eac166c0d2a75e673fa13fbf
Ritikpal_63 <ritikpal227@gmail.com>
2026-08-05 03:28:40 +0530

    Upload First Commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      // "http://localhost:5173",
       "https://portfolio-eta-three-63.vercel.app",
     ],
     credentials: true,
