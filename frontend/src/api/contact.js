6300e26e57a2e81235576f86387898a42d88e786
Ritikpal_63 <ritikpal227@gmail.com>
2026-08-07 03:46:21 +0530

    Upload First Commit

--- frontend/src/api/contact.js
+++ frontend/src/api/contact.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 console.log("API URL", API_BASE_URL);
 
 export const sendContactMessage = async (formData) => {
