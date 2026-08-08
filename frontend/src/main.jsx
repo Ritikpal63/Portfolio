e13fd377f9e9798d04b7d1924e70c2fc18cfe62d
Ritikpal_63 <ritikpal227@gmail.com>
2026-08-08 15:10:47 +0530

    Update Mail Issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
+import App from "./App.jsx";
 import "./index.css";
-import App from "./Salam";
+// import App from "./Salam";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
